309b8b25a6382dbffdc9932dc9e216fdbf74906f
rouslan@chromium.org <rouslan@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2014-06-12 20:21:59 +0000

    Revert 276779 "Move adb_profile_chrome under tools/android/"
    Appears to have broken
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Full/builds/5246
    
      src/tools/android/adb_profile_chrome/main.py: Has executable bit but
      not shebang or ELF header.
    
    Please add the shebang and reland. Sorry for the inconvenience.
    
    > Move adb_profile_chrome under tools/android/
    >
    > Move adb_profile_chrome from build/android/ to tools/android/ to make
    > its purpose clearer.
    >
    > BUG=375754
    > TEST=tools/android/adb_profile_chrome/run_tests
    >
    > Review URL: https://codereview.chromium.org/310413003
    
    TBR=skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/330713002
    
    git-svn-id: http://src.chromium.org/svn/trunk/src/build@276784 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- android/adb_profile_chrome.py
+++ android/adb_profile_chrome.py
@@ -1,8 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env python
 #
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# Start / stop profiling in chrome.
-exec $(dirname $0)/adb_profile_chrome.py $@
+
+import sys
+
+from chrome_profiler import main
+
+
+if __name__ == '__main__':
+  sys.exit(main.main())
