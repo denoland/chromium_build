864a01758ca6fbef0c0a9cc7263de95ff67c2a7e
Eric Stevenson <estevenson@chromium.org>
2020-01-27 22:26:47 +0000

    Trichrome: Add docs for synchronized proguard.
    Bug: 901465
    Change-Id: Icdf67d1c56eb675707eec256c5bfb5ffd6b9a875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2023231
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: (gone) Eric Stevenson <estevenson@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#735597}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: deb7f218c775877f0fc5d1c41e19751d17a415ed

--- android/gyp/write_build_config.py
+++ android/gyp/write_build_config.py
@@ -186,6 +186,14 @@ file corresponding to the content of `deps_info['r_text']`. This is *always*
 generated from the content of `deps_info['r_text']` by the
 `build/android/gyp/process_resources.py` script.
 
+* `deps_info['static_library_dependent_classpath_configs']`:
+Sub dictionary mapping .build_config paths to lists of jar files. For static
+library APKs, this defines which input jars belong to each
+static_library_dependent_target.
+
+* `deps_info['static_library_proguard_mapping_output_paths']`:
+Additional paths to copy the ProGuard mapping file to for static library
+APKs.
 
 ## <a name="target_android_assets">Target type `android_assets`</a>:
 
