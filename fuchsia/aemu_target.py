67cd3fb4c08375e64f1af955dc1543c532495b9f
Chong Gu <chonggu@google.com>
2020-02-01 03:27:25 +0000

    [Fuchsia] Remove dependency of checkout_aemu variable.
    Include AEMU by default in fuchsia package runner scripts for x64 hosts.
    Download AEMU package automatically for mac and linux hosts.
    
    Bug: 1000907
    Change-Id: Ia0ef798a72ba517616331d8a2dcc9b003b180546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2026424
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Chong Gu <chonggu@google.com>
    Cr-Original-Commit-Position: refs/heads/master@{#737570}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 7d2dd6e96f441ae73b43d5ca87d0ebcf7c177b4f

--- fuchsia/aemu_target.py
+++ fuchsia/aemu_target.py
@@ -25,13 +25,9 @@ class AemuTarget(qemu_target.QemuTarget):
     self._enable_graphics = enable_graphics
     self._hardware_gpu = hardware_gpu
 
-  # TODO(bugs.fuchsia.dev/p/fuchsia/issues/detail?id=37301): Remove
-  # once aemu is part of default fuchsia build
   def _EnsureEmulatorExists(self, path):
     assert os.path.exists(path), \
-          'This checkout is missing %s. To check out the files, add this\n' \
-          'entry to the "custon_vars" section of your .gclient file:\n\n' \
-          '   "checkout_aemu": True\n\n' % (self._emu_type)
+          'This checkout is missing %s.' % (self._emu_type)
 
   def _BuildCommand(self):
     aemu_folder = GetEmuRootForPlatform(self._emu_type)
