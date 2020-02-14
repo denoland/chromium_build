b3e662dde161eb950c2a656a323f95a522a9336a
Sajjad Mirza <sajjadm@google.com>
2020-02-14 02:37:39 +0000

    [code coverage] Add files to exclusion list to unblock a CL.
    TBR=liaoyuke@chromium.org
    
    Bug: 1051561
    Change-Id: I6bf84e01a7bbc3c9553d5fb8a29524004df5af0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2056205
    Reviewed-by: Sajjad Mirza <sajjadm@chromium.org>
    Commit-Queue: Sajjad Mirza <sajjadm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#741325}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 836033f9f39dbdc2a4a2a6dfdb2033b96f0846d6

--- toolchain/clang_code_coverage_wrapper.py
+++ toolchain/clang_code_coverage_wrapper.py
@@ -93,6 +93,11 @@ _COVERAGE_EXCLUSION_LIST_MAP = {
         '../../components/cast_channel/cast_channel_enum.cc',
         '../../components/cast_channel/cast_message_util.cc',
         '../../ui/events/keycodes/dom/keycode_converter.cc',
+        # TODO(crbug.com/1051561): angle_unittests affected by coverage.
+        '../../base/message_loop/message_pump_default.cc',
+        '../../base/message_loop/message_pump_libevent.cc',
+        '../../base/message_loop/message_pump_win.cc',
+        '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
     ],
     'chromeos': [
         # These files caused clang to crash while compiling them. They are
@@ -101,6 +106,11 @@ _COVERAGE_EXCLUSION_LIST_MAP = {
         '../../third_party/icu/source/common/uts46.cpp',
         '../../third_party/icu/source/common/ucnvmbcs.cpp',
         '../../base/android/android_image_reader_compat.cc',
+        # TODO(crbug.com/1051561): angle_unittests affected by coverage.
+        '../../base/message_loop/message_pump_default.cc',
+        '../../base/message_loop/message_pump_libevent.cc',
+        '../../base/message_loop/message_pump_win.cc',
+        '../../base/task/sequence_manager/thread_controller_with_message_pump_impl.cc',  #pylint: disable=line-too-long
     ],
     'win': [],
 }
