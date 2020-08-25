29e28633618bca85dcf0137c0a96f19132a3b33a
Nelson Billing <nbilling@google.com>
2020-08-25 04:39:15 +0000

    Implement DWARF Fission support in Chrome Android.
    - This is a fixed version of
    https://chromium-review.googlesource.com/c/chromium/src/+/1995743
    which was reverted because it caused a build failure for ToTLinux.
    -Build failure in ToTLinux is fixed by only adding "--generate-dwp" for
    the Android toolchain when building for Android OS.
    - Modify some compiler build definitions to allow symbol_level=2 with
    Chrome Android 32bit, and to make the build respect use_debug_fission in
    those cases.
    - Modify "link" and "solink" tools in Android toolchain so that DWP
    files can be generated for builds with use_debug_fission.
    - Modify partitioned_shared_library template/script so that DWP files
    can be generated for partitioned libs when use_debug_fission is set.
    
    Change-Id: I7f023de42e46aca1e8c56c9edf3858617e2ab5ad
    
    Cq-Include-Trybots: chromium/try:mac_chromium_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_cfi_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_asan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux_chromium_chromeos_msan_rel_ng
    Cq-Include-Trybots: chromium/try:linux-chromeos-dbg,win-asan
    Cq-Include-Trybots: chromium/try:chromeos-amd64-generic-cfi-thin-lto-rel
    Cq-Include-Trybots: chromium/try:linux_chromium_compile_dbg_32_ng
    Cq-Include-Trybots: chromium/try:win7-rel,win-angle-deqp-rel-32
    Cq-Include-Trybots: chromium/try:linux_angle_deqp_rel_ng
    Cq-Include-Trybots: chromium/try:win-angle-deqp-rel-64
    Cq-Include-Trybots: chromium/try:dawn-win10-x86-deps-rel
    Cq-Include-Trybots: chrome/try:linux-chromeos-chrome
    Cq-Include-Trybots: chrome/try:mac-chrome
    Change-Id: I7f023de42e46aca1e8c56c9edf3858617e2ab5ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2330810
    Commit-Queue: Nelson Billing <nbilling@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#801271}
    GitOrigin-RevId: 3ed7301e9e05897bc8d087e73fd3b0b44f7b555e

--- toolchain/gcc_solink_wrapper.py
+++ toolchain/gcc_solink_wrapper.py
@@ -93,6 +93,7 @@ def main():
   parser.add_argument('--strip',
                       help='The strip binary to run',
                       metavar='PATH')
+  parser.add_argument('--dwp', help='The dwp binary to run', metavar='PATH')
   parser.add_argument('--sofile',
                       required=True,
                       help='Shared object file produced by linking command',
@@ -121,6 +122,16 @@ def main():
   # https://crbug.com/954311 tracks finding a better way to plumb these.
   link_only = InterceptFlag('--link-only', args.command)
   collect_inputs_only = InterceptFlag('--collect-inputs-only', args.command)
+  generate_dwp = InterceptFlag('--generate-dwp', args.command)
+
+  # First, run the actual link.
+  command = wrapper_utils.CommandToRun(args.command)
+  result = wrapper_utils.RunLinkWithOptionalMapFile(command,
+                                                    env=fast_env,
+                                                    map_file=args.map_file)
+
+  if result != 0:
+    return result
 
   # If only linking, we are likely generating a partitioned .so that will be
   # split apart later. In that case:
@@ -158,6 +169,15 @@ def main():
   if result != 0 or link_only:
     return result
 
+  # If dwp is set, then package debug info for this SO.
+  dwp_proc = None
+  if generate_dwp:
+    if not args.dwp:
+      parser.error('--generate-dwp requireds --dwp')
+    dwp_proc = subprocess.Popen(
+        wrapper_utils.CommandToRun(
+            [args.dwp, '-e', args.sofile, '-o', args.output + '.dwp']))
+
   # Next, generate the contents of the TOC file.
   result, toc = CollectTOC(args)
   if result != 0:
@@ -171,6 +191,11 @@ def main():
   if args.strip:
     result = subprocess.call(wrapper_utils.CommandToRun(
         [args.strip, '-o', args.output, args.sofile]))
+
+  if dwp_proc:
+    dwp_result = dwp_proc.wait()
+    if dwp_result != 0:
+      return dwp_result
 
   return result
 
